649724b6e46128435f343d551f28f5f3460fa099
Julien Jakubowski <julien@jakubowski.fr>
2014-03-26 19:26:12 +0100

    use localStorage instead of JSON from AngularJS scope so there can be a standalone component

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,6 @@
 angular.module('testMarvelApp').controller('MainCtrl', function ($scope, marvelService) {
 
 
-    console.log('******* BEFORE');
-
     var ids = [1009521, 1011334, 1017100, 1009144, 1009146, 1016823,
                1009148, 1009149, 1011266, 1010354, 1010846, 1011297];
 
@@ -12,11 +10,8 @@ angular.module('testMarvelApp').controller('MainCtrl', function ($scope, marvelS
     $scope.heros = [];
     for (var i = 0; i < ids.length; i++) {
     	var hero = marvelService.getHero(ids[i]);
-    	console.dir(hero);
     	$scope.heros.push(hero);
     };
-
-    console.log('******* AFTER');
 
     $scope.hire = function(hero) {
     	var index = $scope.heros.indexOf(hero);
