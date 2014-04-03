23fefef0c81f6d42dec62cc788e79c7bbf6bc77c
Romain Linsolas <linsolas@gmail.com>
2014-04-03 12:26:24 +0200

    Simplification

--- app/scripts/super-hero.js
+++ app/scripts/super-hero.js
@@ -1,11 +1,9 @@
 function createAnimation(elt, win) {
-    console.log('Create animation');
     var animation = new Animation(elt, [
         { "box-shadow": "3px 3px 3px darkgrey" },
-        { "box-shadow": "0 0 50px " + (win ? 'green' : 'red') }
+        { "box-shadow": "0 0 50px green" }
     ], {
-        duration: 2,
-        delay: win ? 0 : 0.5
+        duration: 2
     });
     return animation;
 };
